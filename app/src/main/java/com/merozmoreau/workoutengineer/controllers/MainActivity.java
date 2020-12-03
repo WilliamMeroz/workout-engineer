7e17add1ee04389b853eee14617e9927735f3595
JaSONTheMarauder <merozwilliam@protonmail.com>
2020-12-03 08:13:40 -0500

    fixed

--- app/src/main/java/com/merozmoreau/workoutengineer/controllers/MainActivity.java
+++ app/src/main/java/com/merozmoreau/workoutengineer/controllers/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.merozmoreau.workoutengineer.R;
+import com.merozmoreau.workoutengineer.data.ExerciseDa;
+import com.merozmoreau.workoutengineer.data.ExerciseDaCallback;
 import com.merozmoreau.workoutengineer.data.RequestGenerator;
 import com.merozmoreau.workoutengineer.data.RequestGeneratorCallback;
 import com.merozmoreau.workoutengineer.models.Exercise;
@@ -17,17 +19,7 @@ public class MainActivity extends OptionsMenuGeneral {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RequestGenerator r = new RequestGenerator(this);
-        r.fetchExercises(20, new RequestGeneratorCallback() {
-            @Override
-            public void fetchExercisesCallback(List<Exercise> exercises) {
-                for (Exercise e : exercises)
-                    Log.d("test1", e.toString());
-
-                Log.d("test1", "size: " + exercises.size());
-            }
-        });
+        
     }
 
     private void initialize() {
--- app/src/main/java/com/merozmoreau/workoutengineer/data/RequestGenerator.java
+++ app/src/main/java/com/merozmoreau/workoutengineer/data/RequestGenerator.java
@@ -18,18 +18,20 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+// Class used to make HTTP requests to the Wger API. This API returns lists of exercises.
 public class RequestGenerator {
     // Specify that we want to only get exercises in english, since the API is in german by default.
     private final String EXERCISES_URL = "https://wger.de/api/v2/exercise?format=json&language=2&limit=";
-    private Context context;
+    private final Context context;
+    private final Gson gson;
 
     public RequestGenerator(Context context) {
         this.context = context;
+        gson = new Gson();
     }
 
     public void fetchExercises(int numExercises, final RequestGeneratorCallback callback) {
         final List<Exercise> list = new ArrayList<>();
-        final Gson gson = new Gson();
 
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
@@ -49,30 +51,7 @@ public class RequestGenerator {
                                 // Comparing muscle IDs with types.
                                 if (muscleTypeArray.length() > 0) {
                                     int type = muscleTypeArray.getInt(0);
-                                    switch (type) {
-                                        case 1:
-                                        case 11:
-                                            exercise.setType(Exercise.MuscleType.BICEPS);
-                                            break;
-                                        case 2:
-                                            exercise.setType(Exercise.MuscleType.DELTOID);
-                                            break;
-                                        case 8:
-                                            exercise.setType(Exercise.MuscleType.GLUTEUS);
-                                            break;
-                                        case 10:
-                                            exercise.setType(Exercise.MuscleType.QUADRICEPS);
-                                            break;
-                                        case 4:
-                                            exercise.setType(Exercise.MuscleType.PECTORALS);
-                                            break;
-                                        case 9:
-                                            exercise.setType(Exercise.MuscleType.TRAPEZIUS);
-                                            break;
-                                        case 5:
-                                            exercise.setType(Exercise.MuscleType.TRICEPS);
-                                            break;
-                                    }
+                                    exercise.setType(getMuscleType(type));
                                 }
                                 list.add(exercise);
                             }
@@ -92,5 +71,70 @@ public class RequestGenerator {
         );
 
         RequestQueueSingleton.getInstance(context).addToRequestQueue(request);
+    }
+
+    public void fetchExercisesByType(int numExercises, final int type, final RequestGeneratorCallback callback) {
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                EXERCISES_URL + numExercises + "&muscles=" + type,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            List<Exercise> list = new ArrayList<>();
+                            JSONArray array = response.getJSONArray("results");
+
+                            for (int i = 0; i < array.length(); i++) {
+                                Exercise exercise = gson.fromJson(String.valueOf(array.getJSONObject(i)), Exercise.class);
+                                exercise.setType(getMuscleType(type));
+                                list.add(exercise);
+                            }
+
+
+                            callback.fetchExercisesCallback(list);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("test1", error.getMessage());
+                    }
+                }
+        );
+        RequestQueueSingleton.getInstance(context).addToRequestQueue(request);
+    }
+
+    // Since the API uses numbers to distinguish amongst muscle types, we have to associate them to our Enum values.
+    private Exercise.MuscleType getMuscleType(int type) {
+        Exercise.MuscleType muscleType = null;
+        switch (type) {
+            case 1:
+            case 11:
+                muscleType = Exercise.MuscleType.BICEPS;
+                break;
+            case 2:
+                muscleType = Exercise.MuscleType.DELTOID;
+                break;
+            case 8:
+                muscleType = Exercise.MuscleType.GLUTEUS;
+                break;
+            case 10:
+                muscleType = Exercise.MuscleType.QUADRICEPS;
+                break;
+            case 4:
+                muscleType = Exercise.MuscleType.PECTORALS;
+                break;
+            case 9:
+                muscleType = Exercise.MuscleType.TRAPEZIUS;
+                break;
+            case 5:
+                muscleType = Exercise.MuscleType.TRICEPS;
+                break;
+        }
+        return muscleType;
     }
 }
